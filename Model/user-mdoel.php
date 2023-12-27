74e944e488f3ba07322d33bbb1e3d301e9799590
sujit <sujitshrestha0004@gmail.com>
2023-12-27 16:31:49 +0545

    Initial commit

---
+++ Model/user-mdoel.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Model;
+
+class User{
+  public function get(){
+    return "Get method";
+  }
+
+  public function updatw(){
+    return "update method";
+  }
+
+  public function create(){
+    return "create method";
+  }
+  public function delete(){
+    return "delete method";
+  }
+  
+}
+
+?>
