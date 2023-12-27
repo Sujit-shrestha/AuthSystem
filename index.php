7d9cb8ed8d8c7af2fc1c6f67e011f809aafec303
sujit <sujitshrestha0004@gmail.com>
2023-12-27 16:31:57 +0545

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+  switch ($_REQUEST["REQUEST_METHOD"]){
+    case "GET": 
+      
+
+      break;
+
+    case "POST":
+
+      break;
+
+    case "PUT":
+
+      break;
+
+    case "DELETE":
+  }
+
+?>
