ba969003ed5e84bc4c5083e417c1a67ef7de5c33
sujit <sujitshrestha0004@gmail.com>
2023-12-28 22:46:46 +0545

    Created get function

--- Model/user-mdoel.php
+++ Model/user-mdoel.php
@@ -1,10 +1,42 @@
 <?php 
 namespace Model;
+include_once "../Configuration/database-connection.php";
+
+use DbConnection;
 
 class User{
-  public function get(){
-    return "Get method";
+  private $DBconn;
+
+  /* 
+  Dependency injection to use Database connection class properties
+  */
+  public function __construct(DbConnection $DBconn){
+    $this->DBconn  = $DBconn;
   }
+
+  /*
+  @returns array 
+  gets all data from user table
+  */
+  public function get():array{
+    try{
+      $sql = "SELECT * FROM User";     
+
+      $result = $this->DBconn->conn->query($sql);
+
+      $data = $result ->fetch_all(MYSQLI_ASSOC);
+      echo json_encode($data);
+
+      if(empty($data)){
+        throw new \Exception("Unable to fetch data form DB");
+      }else{
+        return $data;
+      }
+   
+  }catch (\Exception $e){
+    return array("error"=> $e->getMessage());
+  }
+}
 
   public function updatw(){
     return "update method";
