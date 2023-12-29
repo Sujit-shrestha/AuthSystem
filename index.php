562d86bf8b0b5d2acdac9914e0b9d806d5b0fea5
sujit <sujitshrestha0004@gmail.com>
2023-12-29 16:33:38 +0545

    login by U&P returns token

--- index.php
+++ index.php
@@ -1,20 +1,59 @@
-<?php 
+<?php
 
-  switch ($_REQUEST["REQUEST_METHOD"]){
-    case "GET": 
-      
+require_once "./Middleware/authentication.php";
+require_once "./Configuration/database-connection.php";
+require_once "./Model/user-mdoel.php";
+require_once "./Configuration/session.php";
 
-      break;
 
-    case "POST":
+use Middleware\JWTTokenHandlerAndAuthentication;
+use Configg\DBConnect;
+use Model\User;
+use Configg\Session;
 
-      break;
 
-    case "PUT":
+switch ($_SERVER["REQUEST_METHOD"]) {
+  case "GET":
 
-      break;
+    break;
 
-    case "DELETE":
-  }
+  case "POST":
+    //
+    if (isset($_POST["username"]) && isset($_POST["password"])) {
+
+      //authenticate user 
+      $authenticationObj = new JWTTokenHandlerAndAuthentication(new User(new DBConnect()));
+
+      $status = $authenticationObj->authenticate($_POST["username"], $_POST["password"]);
+
+      if ($status) {
+        echo "User Authenticated by usernmae password. ";
+
+        //defining payload
+        $payload = array(
+          "username"=> $_POST["username"],
+          "password"=> $_POST["password"],
+        );
+        //creating JWT token 
+
+       $authToken = JWTTokenHandlerAndAuthentication::createToken($payload );
+        
+       
+        //creating Session
+
+        Session::create();
+       setcookie("authToken" , $authToken, time() + 3600,"/");
+
+
+      }
+    }
+    break;
+
+  case "PUT":
+
+    break;
+
+  case "DELETE":
+}
 
 ?>
