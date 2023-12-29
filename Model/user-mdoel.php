66dcca8011c406f3a2b220ca015d61be123e24be
sujit <sujitshrestha0004@gmail.com>
2023-12-29 10:42:20 +0545

    Created create funtion

--- Model/user-mdoel.php
+++ Model/user-mdoel.php
@@ -1,48 +1,72 @@
-<?php 
+<?php
 namespace Model;
+
 include_once "../Configuration/database-connection.php";
 
 use DbConnection;
 
-class User{
+class User
+{
   private $DBconn;
+
+  /**
+   * static funciton to check if data is JSON data
+   * @ param json_data
+   * @return bool
+   */
+
+  public static function isJSON(string $jsonData)
+  {
+    json_decode($jsonData);
+    return (json_last_error() == JSON_ERROR_NONE);
+  }
 
   /* 
   Dependency injection to use Database connection class properties
   */
-  public function __construct(DbConnection $DBconn){
-    $this->DBconn  = $DBconn;
+  public function __construct(DbConnection $DBconn)
+  {
+    $this->DBconn = $DBconn;
   }
 
-  /*
-  @returns array 
-  gets all data from user tablee
-  */
-  public function get():array{
-    try{
-      $sql = "SELECT * FROM User";     
+  /**
+   * @return array 
+   * gets all data from user tablee
+   */
+
+  public function get(): array
+  {
+    try {
+      $sql = "SELECT * FROM User";
 
       $result = $this->DBconn->conn->query($sql);
 
-      $data = $result ->fetch_all(MYSQLI_ASSOC);
+      $data = $result->fetch_all(MYSQLI_ASSOC);
       echo json_encode($data);
 
-      if(empty($data)){
+      if (empty($data)) {
         throw new \Exception("Unable to fetch data form DB");
-      }else{
+      } else {
         return $data;
       }
-   
-  }catch (\Exception $e){
-    return array("error"=> $e->getMessage());
-  }
-}
 
-//updates the database using id as reference
-  public function update($id , $data){
-    try{
-      $data = json_decode($data,true);
-      $sql = "UPDATE User 
+    } catch (\Exception $e) {
+      return array("error" => $e->getMessage());
+    }
+  }
+  /**
+   * updates the database using id as reference
+   * 
+   */
+
+  public function update(int $id, string $data): array
+  {
+    try {
+      if (!User::isJson($data)) {
+        throw new \Exception("The data is not json data.");
+      } else {
+        $data = json_decode($data, true);
+        $sql = "UPDATE User 
       SET email = '$data[email]' ,
           password = '$data[password]' ,
           username = '$data[username]' ,
@@ -51,21 +75,45 @@ class User{
           user_type = '$data[user_type]'
       WHERE id = '$id'
        ";
-      $result = $this->DBconn->conn->query($sql);
-      return $result;
-    
-    }catch (\Exception $e){
-        return array("error" => $e->getMessage()); 
+        $result = $this->DBconn->conn->query($sql);
+        return array("result" => $result);
+      }
+
+    } catch (\Exception $e) {
+      return array("error" => $e->getMessage());
     }
   }
 
-  public function create(){
-    return "create method";
+  /**
+   * Creates new user / Inserts into user table
+   */
+
+  public function create($data)
+  {
+    try {
+      if (!User::isJson($data)) {
+        throw new \Exception("Not json data");
+
+      } else {
+        $sql = "
+        INSERT INTO User 
+      (email , password , username,  name , address , user_type)
+      VALUES
+      ('$data[email]' , '$data[password]' ,'$data[username]' ,'$data[name]' ,'$data[address]' ,'$data[user_type]')
+      ";
+
+        $result = $this->DBconn->conn->query($sql);
+        return $result;
+      }
+    } catch (\Exception $e) {
+      return array("error" => $e->getMessage());
+    }
   }
-  public function delete(){
+  public function delete()
+  {
     return "delete method";
   }
-  
+
 }
 
 ?>
