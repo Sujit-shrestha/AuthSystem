f941045c1583abea116df1100147cd1bec287d5b
sujit <sujitshrestha0004@gmail.com>
2024-01-02 16:30:48 +0545

    created checkpermission function

--- Middleware/authorization.php
+++ Middleware/authorization.php
@@ -4,11 +4,38 @@
 /**
  *
  */
-abstract class Authorization
-{
+
+ interface AuthorizationInterface {
+  public static function checkPermission(string $permission_type) : bool;
+
+ }
+
+ class Authorization implements AuthorizationInterface {
+
+  public static function checkPermission(string $permission_type) : bool{
+    session_start();
+    $user_type = $_SESSION["user_type"]??NULL;
+  
+
+    $permissions = [
+      "" =>[],
+      "admin" => ["edit" , "delete" ] , 
+      "normal" =>["create"]
+    ];
 
 
-}
+    if(in_array($permission_type ,$permissions[$user_type] )){
+/////code remaining
+    return true;
+    }else{
+      return false;
+    }
+
+  }
+ }
+  
+echo Authorization::checkPermission("admin");
+
 
 
 
