a1a31697eaf863cf3501b330b7e74b9f75d3b2bc
sujit <sujitshrestha0004@gmail.com>
2024-01-02 13:22:41 +0545

    created response builder

---
+++ Middleware/response.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Middleware;
+
+class Response {
+
+  public static function respondWithJson($data , $status){
+    header('Content-type : application/json');
+    http_response_code($status);
+    echo json_encode($data);
+  }
+}
+
+
+?>
