56e16a3e059c487aa803acf82b5f1476ed5fad6c
sujit <sujitshrestha0004@gmail.com>
2024-01-03 14:41:04 +0545

    refactor and response standard

--- Routes/login.php
+++ Routes/login.php
@@ -1,34 +1,46 @@
-<?php 
-
+<?php
+namespace Routes;
+use Middleware\Response;
 use Middleware\JWTTokenHandlerAndAuthentication;
 use Configg\DBConnect;
 use Model\User;
 use Configg\Session;
 
- //authenticate user 
- function login(){
-  $authenticationObj = new JWTTokenHandlerAndAuthentication(new User(new DBConnect()));
+//authenticate user 
+class Login
+{
+  public static function login()
+  {
+    $authenticationObj = new JWTTokenHandlerAndAuthentication(new User(new DBConnect()));
 
- $status = $authenticationObj->authenticate($_POST["username"], $_POST["password"]);
+    $status = $authenticationObj->authenticate($_POST["username"], $_POST["password"]);
 
- if ($status) {
-   echo "User Authenticated by usernmae password. ";
-
-   //defining payload
-   $payload = array(
-     "username"=> $_POST["username"],
-     "password"=> $_POST["password"],
-   );
-
-   //creating JWT token 
-  $authToken = JWTTokenHandlerAndAuthentication::createToken($payload);
-   
-  //sets user_type in session
- $_SESSION["login"] = true;
-  setcookie("authToken" , $authToken, time() + 3600,"/");
-
- }
- }
+    if ($status) {
  
+      //defining payload
+      $payload = array(
+        "username" => $_POST["username"],
+        "password" => $_POST["password"],
+        "user_type" => $_SESSION["user_type"],
+        "id"=>$_SESSION["id"] //taking id from session as id is injected when authenticated
+      );
+ 
+      //creating JWT token 
+      $authToken = JWTTokenHandlerAndAuthentication::createToken($payload);
+
+      //sets login true 
+      $_SESSION["login"] = true;
+      setcookie("authToken", $authToken, time() + 3600, "/");
+
+      Response::respondWithJson(array("status"=> "success","authToken"=> $authToken , "message" => "User authenticated successfully.") , 200);
+      
+    }else{
+      setcookie("authToken", "", time() + 3600, "/");
+      Response::respondWithJson(array("status"=> "false","message"=> "Unable to login.") , 403);
+    }
+  }
+}
+
+
 
 ?>
