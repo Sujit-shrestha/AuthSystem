0d3a6aa3b5526e4f58386e086b9207f69d5df991
sujit <sujitshrestha0004@gmail.com>
2024-01-03 14:42:19 +0545

    added getSpecificValueFromToken function

--- Middleware/authentication.php
+++ Middleware/authentication.php
@@ -34,14 +34,21 @@ abstract class Authentication
   public function authenticate($username, $password)
   {
     try {
+     
       $result = $this->userModel->get(null, $username);
 
       if ($result) {
         if (password_verify($password, $result["password"])) {
           //user gets authenticated if code reaches here
+
           //adding user_type into session
           Session::create();
+         
+         
+          $_SESSION["id"] = $result["id"];
           $_SESSION["user_type"] = $result["user_type"];
+         
+          
           return true;
         } else {
           throw new \Exception("Unable to verify the password for given username.");
@@ -67,7 +74,7 @@ class JWTTokenHandlerAndAuthentication extends Authentication
 
   static $token;
   static $secret = "INTUJI_SECRET KEY";
-  static $secretForNormalUser = "PINKUJI_SECRET KEY";
+  // static $secretForNormalUser = "PINKUJI_SECRET KEY";
   static $alg = 'HS256';
 
   /**
@@ -123,9 +130,31 @@ class JWTTokenHandlerAndAuthentication extends Authentication
     }
 
   }
+
+  public static function getSpecificValueFromToken($authToken , $key ){
+    try {
+      
+     
+      $payload = JWT::decode($authToken, new key(static::$secret, static::$alg));
+    
+      return $payload->data->user_type;
+
+  } catch (\Firebase\JWT\ExpiredException $e) {
+    echo "Token Expired";
+    return false;
+
+  } catch (\Firebase\JWT\SignatureInvalidException $e) {
+    echo "Invalid token provided";
+    return false;
+
+  } catch (\Exception $e) {
+    echo '' . $e->getMessage();
+    return false;
+
+  }
+
+  }
+
 }
-
-
-
 
 ?>
