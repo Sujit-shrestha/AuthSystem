49001ece162a32d6723f137fcac0943d9d1803e6
sujit <sujitshrestha0004@gmail.com>
2024-01-11 16:47:45 +0545

    better include method

--- User/Configuration/config.php
+++ User/Configuration/config.php
@@ -1,9 +1,34 @@
 <?php
-namespace Configg;
 
-echo "";
+require_once __DIR__ . "/../vendor/autoload.php";
+foreach (glob(__DIR__ . '/../Routes/*.php') as $file) {
+
+  require_once $file;
+}
+foreach (glob(__DIR__ . '/../Routes/Category/*.php') as $file) {
+
+  require_once $file;
+}
+foreach (glob(__DIR__ . '/../AccessControl/*.php') as $file) {
+  require_once $file;
+}
+foreach (glob(__DIR__ . '/../Configuration/*.php') as $file) {
+  require_once $file;
+}
 
 
+foreach (glob(__DIR__ . '/../Middleware/*.php') as $file) {
+  require_once $file;
+}
+foreach (glob(__DIR__ . '/../Model/*.php') as $file) {
+  require_once $file;
+}
+foreach (glob(__DIR__ . '/../RequestHandlers/*.php') as $file) {
+  require_once $file;
+}
+foreach (glob(__DIR__ . '/../Validate/*.php') as $file) {
+  require_once $file;
 
+}
 
 ?>
