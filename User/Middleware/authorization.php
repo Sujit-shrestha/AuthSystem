a51bff58213510e62145b06bbc1889c225752018
sujit <sujitshrestha0004@gmail.com>
2024-01-12 16:30:12 +0545

    Added verify token into authorization class

--- User/Middleware/authorization.php
+++ User/Middleware/authorization.php
@@ -1,6 +1,8 @@
 <?php
 
-
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\key;
+use Middleware\JWTTokenHandlerAndAuthentication;
 /**
  *
  */
@@ -8,10 +10,49 @@
  interface AuthorizationInterface {
   public static function checkPermission(string $permission_type) : bool;
 
+ public static function verifyToken(string $token):array;
  }
 
  class Authorization implements AuthorizationInterface {
 
+  public static function verifyToken(string $token):array 
+  {
+    try {
+      static $token = $token;
+      static $payload = [];
+      self::$payload = JWT::decode($token, new key(JWTTokenHandlerAndAuthentication::$secret, JWTTokenHandlerAndAuthentication::$alg));
+
+      
+      return [
+        "status" => true,
+        "message" => "User authorised using authToken.",
+        "data" => [
+          "id" => $payload["id"],
+          "username" => $payload["username"],
+          "user_type"=> $payload["user_type"]
+        ]
+      ];
+
+    } catch (\Firebase\JWT\ExpiredException $e) {
+      // echo "Token Expired";
+      error_log($e->getMessage());
+      return [
+        "status"=> false,
+        "message"=> $e->getMessage(),
+        "data" => []
+      ];
+
+    } catch (\Firebase\JWT\SignatureInvalidException $e) {
+      // echo "Invalid token provided";
+      error_log($e->getMessage());
+      return  [
+        "status"=> false,
+        "message"=> $e->getMessage(),
+        "data" => []
+      ];
+    } 
+
+  }
   public static function checkPermission(string $permission_type) : bool{
     session_start();
     $user_type = $_SESSION["user_type"]??NULL;
@@ -20,7 +61,7 @@
     $permissions = [
       "" =>[],
       "admin" => ["PUT" , "DELETE" ] , 
-      "normal" =>["POST"]
+      "employee" =>["POST"]
     ];
 
 
@@ -34,8 +75,6 @@
 
   
  }
-  
-echo Authorization::checkPermission("admin");
 
 
 
