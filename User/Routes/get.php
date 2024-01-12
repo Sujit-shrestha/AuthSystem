9eed01dcc67a2c2adbb2ae06e2e2b7abd7d7295f
sujit <sujitshrestha0004@gmail.com>
2024-01-12 16:21:48 +0545

    refactor

--- User/Routes/get.php
+++ User/Routes/get.php
@@ -1,14 +1,15 @@
-<?php 
-  namespace Routes;
-  
-  use RequestHandlers\RequestHandlers;
-use Middleware\Response;
-  
-  class Get{
-    public static function get(){
-      $response = RequestHandlers::getByIdOrUsername();
-      Response::respondWithJson($response, $response["statusCode"]);
-    }
-  }
+<?php
+namespace Routes;
 
+use RequestHandlers\RequestHandlers;
+use Middleware\Response;
+
+class Get
+{
+  public static function get()
+  {
+    $response = RequestHandlers::getByIdOrUsername();
+    Response::respondWithJson($response, $response["statusCode"]);
+  }
+}
 ?>
