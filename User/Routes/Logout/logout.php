c7072ce0640114b6c15b6bc4ce186e015a7a0025
sujit <sujitshrestha0004@gmail.com>
2024-01-24 16:47:58 +0545

    debugging in progress

--- User/Routes/Logout/logout.php
+++ User/Routes/Logout/logout.php
@@ -7,6 +7,15 @@ use RequestHandlers\LogoutRequestHandlers;
 class Logout
 {
   public static function run(){
-    
+    switch($_SERVER['REQUEST_METHOD']){
+      case 'POST':
+        self::logout();
+        break;
+    }
+  }
+
+  public static function logout(){
+    $response = LogoutRequestHandlers::logout();
+    Response::respondWithJson($response, $response["statusCode"]);
   }
 }
