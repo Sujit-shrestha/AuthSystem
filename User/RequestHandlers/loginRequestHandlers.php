b3445d39c9a38f26ee7e00de38e6dbaafa567c07
sujit <sujitshrestha0004@gmail.com>
2024-01-25 16:08:23 +0545

    created loginRequest handlin g

--- User/RequestHandlers/loginRequestHandlers.php
+++ User/RequestHandlers/loginRequestHandlers.php
@@ -6,50 +6,69 @@ namespace RequestHandlers;
 use Configg\DBConnect;
 use Middleware\JWTTokenHandlerAndAuthentication;
 use Model\User;
-use Middleware\Response;
-
+use Validate\Validator;
 
 class LoginRequestHandlers
 {
-  public static function login(){
+  public static function login()
+  {
     $userObj = new User(new DBConnect());
     $authenticationObj = new JWTTokenHandlerAndAuthentication($userObj);
+    //validation
+    $keys = [
+      "username" => ['required'],
+      "password" => ['required']
+    ];
+    $data = [
+      "username" => $_POST["username"],
+      "password" => $_POST["password"]
+    ];
+    $validationResult = Validator::validate($data, $keys);
 
+
+    if (!$validationResult["validate"]) {
+      return [
+        "status" => "false",
+        "statusCode" => "409",
+        "message" => $validationResult,
+        "data" => $data
+      ];
+    }
     $status = $authenticationObj->authenticate($_POST["username"], $_POST["password"]);
-    if(!$status){
-      $response = [
+    if (!$status) {
+      return [
         "status" => "false",
         "message" => "Unable to authenticate the user.",
         "statusCode" => 401
       ];
-      Response::respondWithJson($response, $response["statusCode"]);
+
     }
-    $userData = $userObj->get(NULL , $_POST["username"]);
-      //defining payload
-      $payload = array(
-        "user_type" =>  $userData["user_type"],
-        "id" => $userData["id"],
-      );
-      //creating JWT token 
-      $authToken = JWTTokenHandlerAndAuthentication::createToken($payload);
+    $userData = $userObj->get(NULL, $_POST["username"]);
+    //defining payload
+    $payload = array(
+      "user_type" => $userData["user_type"],
+      "id" => $userData["id"],
+    );
+    //creating JWT token 
+    $authToken = JWTTokenHandlerAndAuthentication::createToken($payload);
 
-      //storing access_token in session
-      session_start();
-      $_SESSION["authToken"] = $authToken;
-      session_write_close();
+    //storing access_token in session
+    session_start();
+    $_SESSION["authToken"] = $authToken;
+    session_write_close();
 
-      // print_r($_SESSION);
-      $respose_payload = [
-        "access_token" => $authToken ,
-        "user_id"=> $userData["id"],
-        "user_type" =>$userData["user_type"]
-      ];
+    // print_r($_SESSION);
+    $respose_payload = [
+      "access_token" => $authToken,
+      "user_id" => $userData["id"],
+      "user_type" => $userData["user_type"]
+    ];
 
-       return [
-        "status" => true,
-        "message" => "User authenticated successfully.",
-        "payload" => $respose_payload
-      ];
+    return [
+      "status" => true,
+      "message" => "User authenticated successfully.",
+      "payload" => $respose_payload
+    ];
   }
 
 }
