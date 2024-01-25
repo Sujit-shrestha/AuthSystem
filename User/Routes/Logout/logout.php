4e8838a8e26233b87e7d23664992d637827e6a83
sujit <sujitshrestha0004@gmail.com>
2024-01-25 16:12:23 +0545

    routing for logout

--- User/Routes/Logout/logout.php
+++ User/Routes/Logout/logout.php
@@ -1,20 +1,27 @@
-<?php 
+<?php
 
 namespace Routes\Logout;
+
 use Middleware\Response;
 use RequestHandlers\LogoutRequestHandlers;
 
 class Logout
 {
-  public static function run(){
-    switch($_SERVER['REQUEST_METHOD']){
+  public static function run()
+  {
+    switch ($_SERVER['REQUEST_METHOD']) {
       case 'POST':
         self::logout();
+        break;
+        
+      default:
+        echo "Route for given request type not found!!";
         break;
     }
   }
 
-  public static function logout(){
+  public static function logout()
+  {
     $response = LogoutRequestHandlers::logout();
     Response::respondWithJson($response, $response["statusCode"]);
   }
